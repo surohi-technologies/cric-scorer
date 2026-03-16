e3309bf80f4ca97a0c1cda9d76d8c8758fa9a1b0
surohi-technologies <surohitech@gmail.com>
2026-03-16 19:27:10 +0530

    Initial commit

---
+++ src/main/java/com/surohi/cric_scorer/CricScorerApplication.java
@@ -0,0 +1,13 @@
+package com.surohi.cric_scorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricScorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricScorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surohi/cric_scorer/CricScorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surohi.cric_scorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricScorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
